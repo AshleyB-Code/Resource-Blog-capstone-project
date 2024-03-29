e02f0e56afdeb463aaa5a6e2599359ffb25599d5
AshleyB-Code <134429084+AshleyB-Code@users.noreply.github.com>
2024-03-29 01:17:00 -0400

    Add files via upload
    adding things together

--- nav.js
+++ nav.js
@@ -1,17 +1,50 @@
 const mainNav = document.querySelector('.main-nav');
+
+
+
 const navToggle = document.querySelector('.mobile-menu');
 
 navToggle.addEventListener('click', () => {
-    const visibility = mainNav.getAttribute('data-visible');
+    //const mobileLink = document.querySelectorAll('.mobile-link');
+    //const  mobileNav= mobileLink.getAttribute('data-open');
+   
+const visibility = mainNav.getAttribute('data-visible')
     
 if (visibility === "false" ) {
      mainNav.setAttribute('data-visible', true);
-     navToggle.setAttribute('area-expanded', true);
+     navToggle.setAttribute('aria-expanded', true);
+     mobileNav.setAttribute('data-open', true);
+   
+     
 } else if (visibility === "true" ) {
     mainNav.setAttribute('data-visible', false);
-    navToggle.setAttribute('area-expanded',false);
+    navToggle.setAttribute('aria-expanded',false);
+    mobileNav.setAttribute('data-open', false);
+    
 }
 
+
+   
 });
+
+
+
+
+
+
+
+
+
+
+    //const closeLink= document.querySelectorAll('#mobile-link');
+  //closeLink.addEventListener('click', (e) => {
+    //const closeMenu = closeLink.setAttribute('area-expanded', false);
+   // if (closeMenu === 'false'){
+       // navToggle.close(); }
+
+
+  //});
+
+
 
 
